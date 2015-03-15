c618c2e48ef1c0233c67f2e509756cfb4002305a
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-03-14 21:19:55 -0400

    Starting another example.

---
+++ scripts/process_csv_as_schema.scala
@@ -0,0 +1,3 @@
+// This sample assumes that the data files needed are available at the relative path
+ val sp500Cos = sc.textFile("data/constituents-financials.csv")
+
--- scripts/process_csv_as_text.scala
+++ scripts/process_csv_as_text.scala
@@ -1,10 +1,19 @@
 // This sample assumes that the data files needed are available at the relative path
  val sp500Cos = sc.textFile("data/constituents-financials.csv")
 
+ // You can define functions to help you do your work 
+ def isHeader(line: String) :Boolean = {
+  line.contains("Symbol,Name,Sector,Price,")
+ }
+                              
+// You need to remove the headers.
+
+ val headerRemoved = sp500Cos.filterNot(x => isHeader(x))
+
  // At any point type :history to see what you'ev typed in the past to get it back to copy
  // or save in a file.
 
-val splitLines = sp500Cos.map(_ split ",")
+val splitLines = headerRemoved.map(_ split ",")
 
 // we still haven't caused anything to happen yet so let's use an action, like takeSample(n)
 
@@ -20,4 +29,4 @@ splitSample.getClass
 
  // This is because we did not use any of Spark's capabilities to support typed data.. 
  // we simply loaded and split the records on the delimiter, naively and in the end 
- // not efficiently.
+ // not efficiently.  Also, we didn't get into skipping the header record, which is easy
