4bd34bedaa08356f0051c514ffe2942109a876cc
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-03-24 22:01:47 -0400

    Added function to create StructType from JDBC ResultSetMeta

--- sparkschemabuilder/src/main/scala/org/invisibletech/MetaDataFacade.scala
+++ sparkschemabuilder/src/main/scala/org/invisibletech/MetaDataFacade.scala
@@ -2,6 +2,9 @@ package org.invisibletech
 
 import scala.collection.mutable.ArrayBuffer
 import java.sql.ResultSetMetaData
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StringType
 
 object MetaDataFacade {
   def names(meta : ResultSetMetaData): Array[String] = {
@@ -12,5 +15,8 @@ object MetaDataFacade {
     
     arr.toArray
   }
-
+  
+  def createStringTypeSchema(names: Array[String]) : StructType  = {
+    StructType(names.map { name => StructField(name, StringType, true)})
+  }
 }
